262916a9f567efad49a737f150e584267772cb26
Renaldinho <rena0968@easv365.dk>
2024-05-23 19:31:39 +0200

    role creation

--- sql-scripts/sql_003.sql
+++ sql-scripts/sql_003.sql
@@ -1,1 +1,9 @@
 ﻿-- creating some roles
+
+INSERT INTO roles (role_name) VALUES ('admin');
+
+INSERT INTO roles (role_name) VALUES ('manager');
+
+INSERT INTO roles (role_name) VALUES ('public-user');
+
+INSERT INTO roles (role_name) VALUES ('base-user');
