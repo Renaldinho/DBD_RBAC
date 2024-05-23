0edaac42392536e3b73d81626b7f9e7a52dfeec7
Renaldinho <rena0968@easv365.dk>
2024-05-23 19:40:25 +0200

    creating users

--- sql-scripts/sql_004.sql
+++ sql-scripts/sql_004.sql
@@ -1,1 +1,30 @@
 ﻿-- assigning permissions to roles
+
+-- Admin Role (all permissions)
+INSERT INTO role_permission_assignments (role_id, permission_id)
+SELECT r.role_id, p.permission_id
+FROM roles r, permissions p
+WHERE r.role_name = 'admin';
+
+-- Manager Role (most permissions, excluding user/database management)
+INSERT INTO role_permission_assignments (role_id, permission_id)
+SELECT r.role_id, p.permission_id
+FROM roles r, permissions p
+WHERE r.role_name = 'manager'
+  AND p.permission_name NOT IN ('create_users', 'edit_users', 'delete_users', 'reset_passwords',
+                                'manage_database', 'backup_database', 'restore_database');
+
+-- Base User (access to basic data and some actions)
+INSERT INTO role_permission_assignments (role_id, permission_id)
+SELECT r.role_id, p.permission_id
+FROM roles r, permissions p
+WHERE r.role_name = 'base-user'
+  AND p.permission_name IN ('create', 'read', 'update', 'view_customer_data', 'generate_reports',
+                            'access_dashboard', 'send_notifications');
+
+-- Public User (very limited access)
+INSERT INTO role_permission_assignments (role_id, permission_id)
+SELECT r.role_id, p.permission_id
+FROM roles r, permissions p
+WHERE r.role_name = 'public-user'
+  AND p.permission_name IN ('read', 'view_customer_data', 'access_dashboard');
--- sql-scripts/sql_005.sql
+++ sql-scripts/sql_005.sql
@@ -1,1 +1,22 @@
 ﻿-- creating some users
+
+
+CREATE EXTENSION pgcrypto; -- Extension for creating password hash for a level of real-ness
+
+-- Creating Admin User
+INSERT INTO users (username, password)
+VALUES ('admin', crypt('admin_password', gen_salt('bf', 8)));
+
+-- Creating Manager User
+INSERT INTO users (username, password)
+VALUES ('manager1', crypt('manager_password', gen_salt('bf', 8)));
+
+-- Creating Base Users
+INSERT INTO users (username, password)
+VALUES ('user1', crypt('user_password', gen_salt('bf', 8)));
+INSERT INTO users (username, password)
+VALUES ('user2', crypt('user_password', gen_salt('bf', 8)));
+
+-- Creating Public User
+INSERT INTO users (username, password)
+VALUES ('guest', crypt('guest_password', gen_salt('bf', 8)));
